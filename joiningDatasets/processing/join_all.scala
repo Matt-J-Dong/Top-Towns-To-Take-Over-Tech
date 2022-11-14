a22173d089f0c130d9f9ee6684254cf0f70d7c03
Ericsson Colborn <evc252@hlog-2.hpc.nyu.edu>
2022-11-14 14:13:11 -0500

    update: joined all datasets, uploaded csv to joiningDatasets/data

--- joiningDatasets/processing/join_all.scala
+++ joiningDatasets/processing/join_all.scala
@@ -16,14 +16,9 @@ val data_dir = "final_project/data/";
 val rent_income = spark.read.option("header", "true").csv(data_dir + "rent_income.csv");
 val weather = spark.read.option("header", "true").csv(data_dir + "weather.csv");
 
-// val good_days = 
 val join_df = rent_income.join(weather, Seq("city", "state"), "inner");
-// select(
-//     col("city"),
-//     col("state")
-// );
 
-// TODO: calculate average of 3 rents
+// TODO: calculate average of 3 rents + average of 3 good days 
 
 join_df.show();
 join_df.write.option("header", "true").csv("final_project/merge_all_output");
