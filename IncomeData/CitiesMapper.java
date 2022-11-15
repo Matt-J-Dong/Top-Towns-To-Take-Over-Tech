fa7b4f95b818b1f653ea4c78e9a417e89365b43d
Matthew Dong <95544597+Matt-J-Dong@users.noreply.github.com>
2022-11-14 19:06:06 -0500

    push finished csv to github

--- IncomeData/CitiesMapper.java
+++ IncomeData/CitiesMapper.java
@@ -20,11 +20,14 @@ public class CitiesMapper extends Mapper<Object, Text, Text, Text>{
         String noQuotes1 = str[0].substring(1,str[0].length()-1);
         String noQuotes2 = str[6].substring(1,str[6].length()-1);
         String noQuotes3 = str[7].substring(1,str[7].length()-1);
+        String noQuotes4 = str[2].substring(1,str[2].length()-1);
         String[] cities = {"Austin","New York","Seattle","San Diego","Houston","Boston"};//could convert to list to use a different method
-        for (String city:cities){
-            if(city.equals(noQuotes1)){
+        String[] states = {"TX","NY","WA","CA","TX","MA"};
+        for (int i =0;i<cities.length;i++){
+            if(cities[i].equals(noQuotes1) && states[i].equals(noQuotes4)){
+                String location = noQuotes1 + ", "+noQuotes4;
                 String latlong = ","+noQuotes2+","+noQuotes3;
-                context.write(new Text(noQuotes1),new Text(latlong));
+                context.write(new Text(location),new Text(latlong));
             }
         }
       }
