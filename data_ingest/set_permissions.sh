950e0d0205387f3cd05305cab46320e199ffd11d
Ericsson Colborn <evc252@hlog-2.hpc.nyu.edu>
2022-11-27 12:16:47 -0500

    fix: added myself to ACL in set_permissions.sh

--- data_ingest/set_permissions.sh
+++ data_ingest/set_permissions.sh
@@ -10,9 +10,11 @@
 # group members
 hdfs dfs -setfacl -m -R default:user:zc1548:rwx /user/evc252/shared_data
 hdfs dfs -setfacl -m -R default:user:mjd9571:rwx /user/evc252shared_data
+hdfs dfs -setfacl -m -R default:user:evc252:rwx /user/evc252shared_data
 
 hdfs dfs -setfacl -m user:zc1548:--x /user/evc252
 hdfs dfs -setfacl -m user:mjd9571:--x /user/evc252
+hdfs dfs -setfacl -m user:evc252:--x /user/evc252
 
 # graders
 
