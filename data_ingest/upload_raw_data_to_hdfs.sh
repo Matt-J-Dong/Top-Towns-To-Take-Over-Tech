63ba9f5d2373b38da10f04dd61d93ed091a0a3f6
Ericsson Colborn <evc252@hlog-2.hpc.nyu.edu>
2022-11-27 23:27:43 -0500

    fixed upload script

--- data_ingest/upload_raw_data_to_hdfs.sh
+++ data_ingest/upload_raw_data_to_hdfs.sh
@@ -3,10 +3,10 @@
 # *******************************************
 #
 # first, delete old files
-hdfs dfs -rm data/raw/raw_rent.csv /user/evc252/shared_data/raw
-hdfs dfs -rm data/raw/raw_weather.csv /user/evc252/shared_data/raw
-hdfs dfs -rm data/raw/raw_income.csv /user/evc252/shared_data/raw
-hdfs dfs -rm data/raw/raw_geo.csv /user/evc252/shared_data/raw 
+hdfs dfs -rm data/raw/raw_rent.csv
+hdfs dfs -rm data/raw/raw_weather.csv
+hdfs dfs -rm data/raw/raw_income.csv
+hdfs dfs -rm data/raw/raw_geo.csv 
 #
 # upload new files
 hdfs dfs -put data/raw/raw_rent.csv /user/evc252/shared_data/raw
