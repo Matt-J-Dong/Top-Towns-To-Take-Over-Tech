1d70c1b2cf9e19347813d13a6f0353d832e41b65
Ericsson Colborn <evc252@hlog-2.hpc.nyu.edu>
2022-11-27 20:03:25 -0500

    update: added professor and grader to  set_permissions.sh

--- data_ingest/set_permissions.sh
+++ data_ingest/set_permissions.sh
@@ -16,7 +16,9 @@ hdfs dfs -setfacl -m user:mjd9571:--x /user/evc252
 hdfs dfs -setfacl -m user:evc252:--x /user/evc252
 
 # graders
-
+hdfs dfs -setfacl -m -R default:user:sj3549:rwx /user/evc252/shared_data
+hdfs dfs -setfacl -m user:sj3549:--x /user/evc252
 
 # professor
-
+hdfs dfs -setfacl -m -R default:user:adm209:rwx /user/evc252/shared_data
+hdfs dfs -setfacl -m user:adm209:--x /user/evc252
